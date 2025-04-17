85b21cdde7e58e0763dc9ba5433980801d5a0f17
Trevor Slocum <trevor@rocket9labs.com>
2025-04-17 10:10:57 -0700

    Set board page title

--- plugin/fortune/fortune.go
+++ plugin/fortune/fortune.go
@@ -10,6 +10,9 @@ import (
 const (
 	configTriggers = "triggers"
 	configFortunes = "fortunes"
+
+	defaultTrigger  = "#fortune"
+	defaultFortunes = `<font color="#B604A2"><b>Your fortune: Godly Luck</b></font>|<font color="indigo"><b>Your fortune: Outlook good</b></font>|<font color="dodgerblue"><b>Your fortune: You will meet a dark handsome stranger</b></font>|<font color="darkorange"><b>Your fortune: Good Luck</b></font>|<font color="royalblue"><b>Your fortune: Better not tell you now</b></font>|<font color="deeppink"><b>Your fortune: Reply hazy; try again</b></font>|<font color="lime"><b>Your fortune: Very Bad Luck</b></font>|<font color="lime"><b>Your fortune: Good news will come to you by mail</b></font>|<font color="#BFC52F"><b>Your fortune: Average Luck</b></font>`
 )
 
 type Fortune struct{}
@@ -22,16 +25,16 @@ func (f *Fortune) Config() []sriracha.PluginConfig {
 	return []sriracha.PluginConfig{
 		{
 			Type:        sriracha.TypeString,
+			Multiple:    true,
 			Name:        configTriggers,
 			Default:     defaultTrigger,
 			Description: "The text users may input in the name or email field to receive a fortune.",
-			Multiple:    true,
 		}, {
 			Type:        sriracha.TypeString,
+			Multiple:    true,
 			Name:        configFortunes,
 			Default:     defaultFortunes,
 			Description: "The fortunes users may receive.",
-			Multiple:    true,
 		},
 	}
 }
@@ -71,11 +74,6 @@ func (f *Fortune) Post(db *sriracha.Database, post *sriracha.Post) error {
 func init() {
 	sriracha.RegisterPlugin(&Fortune{})
 }
-
-const (
-	defaultTrigger  = "#fortune"
-	defaultFortunes = `<font color="#B604A2"><b>Your fortune: Godly Luck</b></font>|<font color="indigo"><b>Your fortune: Outlook good</b></font>|<font color="dodgerblue"><b>Your fortune: You will meet a dark handsome stranger</b></font>|<font color="darkorange"><b>Your fortune: Good Luck</b></font>|<font color="royalblue"><b>Your fortune: Better not tell you now</b></font>|<font color="deeppink"><b>Your fortune: Reply hazy; try again</b></font>|<font color="lime"><b>Your fortune: Very Bad Luck</b></font>|<font color="lime"><b>Your fortune: Good news will come to you by mail</b></font>|<font color="#BFC52F"><b>Your fortune: Average Luck</b></font>`
-)
 
 // Validate plugin interfaces during compilation.
 var (
