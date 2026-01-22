076a851b77cca47a53fd896a49e5e47a32e6548d
Trevor Slocum <trevor@rocket9labs.com>
2026-01-22 13:18:10 -0800

    Add shortcuts to enable and disable all file types

--- static/js/manage.js
+++ static/js/manage.js
@@ -6,3 +6,11 @@ function liftBan(id) {
     document.getElementById('reason' + id).value = reason;
     return true;
 }
+
+function setAllUploads(enabled) {
+    var uploads = document.getElementsByName('uploads');
+    for (var i = 0; i < uploads.length; i++) {
+        uploads[i].checked = enabled;
+    }
+    return false;
+}
