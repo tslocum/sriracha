617ffdc6cfaa301a1f68ce461c136f1298f982ea
Trevor Slocum <trevor@rocket9labs.com>
2026-08-02 19:40:29 -0700

    Buffer build queue

--- internal/server/server.go
+++ internal/server/server.go
@@ -285,6 +285,7 @@ func NewServer() *Server {
 	httpClient := &http.Client{
 		Timeout: 15 * time.Second,
 	}
+	buildQueueSize := runtime.NumCPU() * 2
 	return &Server{
 		captchaCache:   make(map[string]string),
 		keywordCache:   make(map[int][]*cachedKeyword),
@@ -298,7 +299,7 @@ func NewServer() *Server {
 		indexCacheLock:        &sync.Mutex{},
 		lastSearch:            make(map[string]int64),
 		modQueueSize:          -1,
-		buildQueue:            make(chan *buildInfo),
+		buildQueue:            make(chan *buildInfo, buildQueueSize),
 		rebuildQueue:          make(chan *rebuildInfo),
 		httpClient:            httpClient,
 		connCount:             &atomic.Int32{},
